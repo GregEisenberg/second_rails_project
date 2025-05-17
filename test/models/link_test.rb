d8b9be06279b598ffbe1906834329d6be58c4007
Greg Eisenberg <gregeisenberg@hotmail.com>
2025-05-16 21:41:54 -0700

    first tests with minitest - I dont like fixures

--- test/models/link_test.rb
+++ test/models/link_test.rb
@@ -1,7 +1,44 @@
 require "test_helper"
 
+
 class LinkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test "invalid link - needs at least 5 chars in title" do
+      link = User.first().links.new()
+
+      link.title = "four"
+      link.url = "at least five chars"
+
+      result = link.save()
+      assert_not(result)
+    end
+
+    test "valid link - needs at least 5 chars in title" do
+      link = User.first().links.new()
+
+      link.title = "five5"
+      link.url = "at least five chars"
+
+      result = link.save()
+      assert(result)
+    end
+
+    test "invalid link - needs at least 5 chars in url" do
+        link = User.first().links.new()
+
+        link.title = "at least five chars"
+        link.url = "four"
+
+        result = link.save()
+        assert_not(result)
+    end
+
+      test "valid link - needs at least 5 chars in url" do
+        link = User.first().links.new()
+
+        link.title = "at least five chars"
+        link.url = "five5"
+
+        result = link.save()
+        assert(result)
+    end
 end
